f9ba9d420390633f2bda94e451b7ba93409b4432
alibabaD44 <yigitoluk145@gmail.com>
2026-09-01 15:28:10 +0300

    responsive added

--- src/navbar/navbar.jsx
+++ src/navbar/navbar.jsx
@@ -3,8 +3,8 @@ import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 function Navbar() {
-
     const [scrolled, setScrolled] = useState(false)
+    const [menuOpen, setMenuOpen] = useState(false)
 
     useEffect(() => {
         const handleScroll = () => {
@@ -18,27 +18,80 @@ function Navbar() {
         }
     }, [])
 
+    const closeMenu = () => {
+        setMenuOpen(false)
+    }
+
     return (
-        <>
-            <nav className={scrolled ? 'scrolled' : ''}>
+        <nav className={scrolled ? 'scrolled' : ''}>
 
-                <div className='nav-sec-1'>
-                    <Link to="/">
-                        <img src="Logo_WHITE.png" alt="SEHREMINIMUN" />
-                    </Link>
-                </div>
+            <div className="nav-sec-1">
+                <Link to="/" onClick={closeMenu}>
+                    <img
+                        src="Logo_WHITE.png"
+                        alt="SEHREMINIMUN"
+                    />
+                </Link>
+            </div>
 
-                <div className='nav-sec-2'>
-                    <Link to="/applications">Applications</Link>
-                    <Link to="/our-team">Our Team</Link>
-                    <Link to="/committees">Committees</Link>
-                    <Link to="/conference">Conference</Link>
-                    <Link to="/schedule">Schedule</Link>
-                    <Link to="/faq">FAQ</Link>
-                </div>
+            <div className={`nav-sec-2 ${menuOpen ? 'open' : ''}`}>
 
-            </nav>
-        </>
+                <Link
+                    to="/applications"
+                    onClick={closeMenu}
+                >
+                    Applications
+                </Link>
+
+                <Link
+                    to="/our-team"
+                    onClick={closeMenu}
+                >
+                    Our Team
+                </Link>
+
+                <Link
+                    to="/committees"
+                    onClick={closeMenu}
+                >
+                    Committees
+                </Link>
+
+                <Link
+                    to="/conference"
+                    onClick={closeMenu}
+                >
+                    Conference
+                </Link>
+
+                <Link
+                    to="/schedule"
+                    onClick={closeMenu}
+                >
+                    Schedule
+                </Link>
+
+                <Link
+                    to="/faq"
+                    onClick={closeMenu}
+                >
+                    FAQ
+                </Link>
+
+            </div>
+
+            <button
+                className={`menu-toggle ${menuOpen ? 'active' : ''}`}
+                onClick={() => setMenuOpen(prev => !prev)}
+                aria-label="Toggle navigation menu"
+                aria-expanded={menuOpen}
+            >
+                <span></span>
+                <span></span>
+                <span></span>
+            </button>
+
+        </nav>
     )
 }
 
